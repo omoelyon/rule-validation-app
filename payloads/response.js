a7d07fdf95a37fd6e7227ba812dd3931660c28d6
omoelyon <ganiyuelegbede@yahoo.com>
2021-01-30 04:06:29 +0100

    removed twitter handle

--- payloads/response.js
+++ payloads/response.js
@@ -2,8 +2,7 @@ data = {
     name: "Adeola elegbede",
     github: "@omoelyon",
     email: "adeolae53@gmail.com",
-    mobile: "07035592428",
-    twitter: ""
+    mobile: "07035592428"
   }
 
 exports.base =      data;   
